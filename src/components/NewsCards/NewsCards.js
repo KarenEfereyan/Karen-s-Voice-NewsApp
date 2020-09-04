b9050d469b9e2f31284c8a170e5ced2e709302e1
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-09-04 14:47:38 +0100

    News Card Component complete

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Grid, Grow, Typography } from "@material-ui/core";
 import NewsCard from "../NewsCard/NewsCard";
+import useStyles from "./styles.js";
 
 const NewsCards = ({ articles, i }) => {
+  const classes = useStyles();
+
   return (
     <Grow in>
-      <Grid container alignItems="stretch" spacing={3}>
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
         {articles.map((article, i) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-            styles={{ display: "flex" }}
-          ></Grid>
+          <Grid item xs={12} sm={6} md={4} lg={3} styles={{ display: "flex" }}>
+            <NewsCard article={article} i={i} />
+          </Grid>
         ))}
       </Grid>
     </Grow>
