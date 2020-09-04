63cf0add584a0de50dfbc2d2cbcfcb644ed6b385
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-09-05 00:40:20 +0100

    First complete app iteration done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from './components/NewsCards/NewsCards';
+//import Modal from './components/Modal/Modal';
+import wordsToNumbers from 'words-to-numbers';
+import useStyles from './styles.js';
 import { Typography } from '@material-ui/core';
+
+
 
 //This allows me interact with my ALAN studio project
 const alanKeyId =
@@ -10,17 +15,37 @@ const newsId='f28a3f24a9aa43c0bd5ef839bce4eb26';
 
 
 const App = () => {
-  // we will be setting the articles here
+  // We will be setting the articles and active articles here
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+
+ const classes = useStyles();
 
   //Once the  app component is loaded, I want to console log the gotten articles
+
   useEffect(() => {
     alanBtn({
-      key: alanKeyId,
-      onCommand: ({ command, articles }) => {
-        if (command === "newHeadlines") {
+      key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
           setNewsArticles(articles);
-          //console.log('From the app component', articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
       },
     });
@@ -28,8 +53,17 @@ const App = () => {
 
   return (
     <div>
-    <h1 className="header">Get Your News Via Voice</h1>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      {/* <Modal isOpen={isOpen} setIsOpen={setIsOpen} /> */}
     </div>
   );
 };
