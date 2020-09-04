dcf4c2f0fa2b7ffd72961ca49c73723ea528e944
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-09-04 08:03:37 +0100

    Simple UI for individual grid items

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -4,29 +4,20 @@ import NewsCard from "../NewsCard/NewsCard";
 
 const NewsCards = ({ articles, i }) => {
   return (
-    <div>
-      {articles.map((article, i) =>(
-        <NewsCard />
-      )
-
-      )}
-    </div>
-    // <div>
-    //   <Grow in>
-    //     <Grid container alignItems="stretch" spacing={3}>
-    //       {articles.map((article, i) => {
-    //         <Grid
-    //           item
-    //           xs={12}
-    //           sm={6}
-    //           md={4}
-    //           lg={3}
-    //           styles={{ display: "flex" }}
-    //         ></Grid>;
-    //       })}
-    //     </Grid>
-    //   </Grow>
-    // </div>
+    <Grow in>
+      <Grid container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            styles={{ display: "flex" }}
+          ></Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 };
 
