9beee100b97562b401c869a8ff45410c523ff9e2
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-09-04 16:37:12 +0100

    Added newscards component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import NewsCards from "./components/NewsCards/NewsCards";
+import NewsCards from './components/NewsCards/NewsCards';
 
+//This allows me interact with my ALAN studio project
 const alanKeyId =
-  "e9da2f327508022e3b8190d645cafa212e956eca572e1d8b807a3e2338fdd0dc/stage";
-const newsApiId = "";
+  'e9da2f327508022e3b8190d645cafa212e956eca572e1d8b807a3e2338fdd0dc/stage'
+const newsId='f28a3f24a9aa43c0bd5ef839bce4eb26';
 
 const App = () => {
-  const [newsArticles, setNewsArticles] = useState([0]);
+  // we will be setting the articles here
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  //Once the  app component is loaded, I want to console log the gotten articles
   useEffect(() => {
     alanBtn({
       key: alanKeyId,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
+          //console.log('From the app component', articles);
         }
       },
     });
@@ -21,7 +26,6 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan News App</h1>
       <NewsCards articles={newsArticles} />
     </div>
   );
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,27 +1,29 @@
-import React from "react";
-import { Grid, Grow, Typography } from "@material-ui/core";
-import NewsCard from "../NewsCard/NewsCard";
-import useStyles from "./styles.js";
+import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
+import useStyles from './styles.js';
 
-const NewsCards = ({ articles, i }) => {
-  const classes = useStyles();
+const NewsCards = ({articles}) => {
+//allows us use css in js
+    const classes = useStyles();
+    console.log("From the news app component", articles);
+    if(!articles.length){
+        return(
+            <h1>There are no news available</h1>
+        )
+    }
 
-  return (
-    <Grow in>
-      <Grid
-        className={classes.container}
-        container
-        alignItems="stretch"
-        spacing={3}
-      >
-        {articles.map((article, i) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} styles={{ display: "flex" }}>
-            <NewsCard article={article} i={i} />
-          </Grid>
-        ))}
-      </Grid>
-    </Grow>
-  );
-};
+    return (
+        <Grow in>
+        <Grid className = {classes.container} container alignItems="stretch" spacing={3}>
+          {articles.map((article, i) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <span>test</span>
+              {/* <NewsCard activeArticle={activeArticle} i={i} article={article} /> */}
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
+    );
+}
 
-export default NewsCards;
+export default NewsCards
